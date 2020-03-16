6ef143b64a5ca9475845a14dc1821a96e7165cfd
Leng <jerome.leng@etud.univ-angers.fr>
2020-03-16 11:44:20 +0100

    Rename all files

--- src/main.java
+++ src/main.java
@@ -1,9 +1,39 @@
+import java.io.*;
 
 public class main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void affiche() throws IOException {
+		String ligne = "";
+		String fichier = "/home/etudiant/Documents/eclipse/Opti_Combin/Data/Base1.txt";
+		BufferedReader clavier = new BufferedReader(new InputStreamReader(
+				System.in));
 
+		BufferedReader ficTexte;
+		try {
+			ficTexte = new BufferedReader(new FileReader(new File(fichier)));
+			if (ficTexte == null) {
+				throw new FileNotFoundException("Fichier non trouvé: "
+						+ fichier);
+			}
+			do {
+				ligne = ficTexte.readLine();
+				if (ligne != null) {
+					System.out.println(ligne);
+				}
+			} while (ficTexte != null);
+			ficTexte.close();
+			System.out.println("\n");
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		affiche();
 	}
 
 }
